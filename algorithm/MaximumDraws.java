951ce84a102ef10d99fc6bab83a7d19232a816d5
youjin.choi <dev.youjin.choi@gmail.com>
2018-01-12 18:02:00 +0900

    [해커랭크]Maximum Draws

---
+++ algorithm/MaximumDraws.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/*
+* https://www.hackerrank.com/challenges/maximum-draws/problem
+* */
+public class MaximumDraws {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		for(int i = 0; i < T; i++){
+			int n = in.nextInt();
+			System.out.println(n + 1);
+		}
+	}
+}
