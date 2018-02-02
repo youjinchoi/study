1a8bff4a14db2f5f4b9b460a36f4905aa614b87f
youjin.choi <dev.youjin.choi@gmail.com>
2018-02-02 18:33:07 +0900

    [해커랭크]Drawing Book

---
+++ algorithm/DrawingBook.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/*
+* https://www.hackerrank.com/challenges/drawing-book/problem
+* */
+public class DrawingBook {
+
+	static int solve(int n, int p){
+		int pagesFromFirst = p/2;
+		int pagesFromLast = n/2 - pagesFromFirst;
+		return pagesFromFirst < pagesFromLast ? pagesFromFirst : pagesFromLast;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int p = in.nextInt();
+		int result = solve(n, p);
+		System.out.println(result);
+	}
+}
