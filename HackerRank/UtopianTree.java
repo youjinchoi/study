fc2e96eceed5d283c118ef7abab5e3f358f70077
youjin.choi <dev.youjin.choi@gmail.com>
2018-02-06 20:58:21 +0900

    [해커랭크]Maximizing XOR, Utopian Tree

---
+++ HackerRank/UtopianTree.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+/*
+* https://www.hackerrank.com/challenges/utopian-tree/problem
+* */
+public class UtopianTree {
+
+	static int utopianTree(int n) {
+		int growth = 1;
+		for (int i=1; i<=n; i++) {
+			if (i % 2 == 1) {
+				growth *= 2;
+			} else {
+				growth += 1;
+			}
+		}
+		return growth;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for(int a0 = 0; a0 < t; a0++){
+			int n = in.nextInt();
+			int result = utopianTree(n);
+			System.out.println(result);
+		}
+		in.close();
+	}
+}
